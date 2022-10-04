afbf30c0074c9943157e29c2037225b91f570d9a
Max Zakharzhevskiy <maxzakh@ucdavis.edu>
2022-10-03 22:24:03 -0400

    WIP. add test images

---
+++ src/store/gallery-data.ts
@@ -0,0 +1,13 @@
+import testDog from '../assets/test-images/test-dog.jfif';
+import testHorse from '../assets/test-images/test-horse.jfif';
+import testMouses from '../assets/test-images/test-mouses.jfif';
+import testOwl from '../assets/test-images/test-owl.jfif';
+import testPanda from '../assets/test-images/test-panda.jfif';
+
+export const galleryImages = [
+    testDog,
+    testHorse,
+    testMouses,
+    testOwl,
+    testPanda,
+];
